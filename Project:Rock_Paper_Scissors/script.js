7fc90243a7d8a9b7ff7f433dc2c5ac6ac8191609
Arya Gill <aryagill43@icloud.com>
2022-12-26 15:27:19 -0800

    Create playRound function

--- Project:Rock_Paper_Scissors/script.js
+++ Project:Rock_Paper_Scissors/script.js
@@ -5,3 +5,19 @@ function getComputerChoice() {
     return(choices[randomIndex]);
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    )  {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+}
+

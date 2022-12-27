e3a2d86ffc4b36740a0cbc3df23c170268feb2c7
Arya Gill <aryagill43@icloud.com>
2022-12-26 17:06:56 -0800

    Create the function game() and add a scoring mechanism

--- Project-Rock_Paper_Scissors/script.js
+++ Project-Rock_Paper_Scissors/script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissor"];
+let cpuScore = 0;
+let myScore = 0;
 
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random()*choices.length);
@@ -11,12 +13,14 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "Tie!";
     } else if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "rock" && playerSelection == "scissor") ||
+        (computerSelection == "scissor" && playerSelection == "paper") ||
         (computerSelection == "paper" && playerSelection == "rock")
     )  {
+        cpuScore++;
         return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
     } else {
+        myScore++;
         return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
     }
 }
@@ -25,3 +29,24 @@ function capitalize(str){
     return (str[0].toUpperCase() + str.slice(1));
 }
 
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter Your choice", "Rock , Paper, Scissor").toLowerCase();
+        if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissor") {
+            alert("Option not recognized! Make sure the answer is either Rock, Paper, or Scissor")
+            playerSelection = prompt("Enter Your choice", "Rock , Paper, Scissor").toLowerCase();
+        } else {
+            console.log(playRound(playerSelection, getComputerChoice()));
+        }
+    }
+    if (myScore > cpuScore) {
+        console.log("You Won!");
+        alert("You Won!");
+    } else if (myScore < cpuScore){
+        console.log("Mission failed we'll get em next time");
+        alert("Mission failed we'll get em next time");
+    } else {
+        console.log("FINAL: Tie!");
+        alert("Tie!")
+    }
+}

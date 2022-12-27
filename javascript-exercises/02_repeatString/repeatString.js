ab932a9e156bbfc997d51cd84d371b119d744020
Arya Gill <aryagill43@icloud.com>
2022-12-27 14:27:10 -0800

    Solve 02_repeatString

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+// const repeatString = (str, num) => {
+//     if (num < 0) return 'ERROR';
+//     return str.repeat(num);
+//   };
+  
+//   module.exports = repeatString;
 
-};
+// ------OR------
+const repeatString = (str, num) => {
+    if (num < 0) return 'ERROR';
+    if (num === 0) return '';
+    let result = '';
+    for (let i = 0; i < num; i++) {
+      result += str;
+    }
+    return result;
+  };
 
-// Do not edit below this line
-module.exports = repeatString;
+  module.exports = repeatString;

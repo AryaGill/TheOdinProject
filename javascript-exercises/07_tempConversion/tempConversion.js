803bd0637383dfdff510d8d29829d40d3b6071d0
Arya Gill <aryagill43@icloud.com>
2022-12-30 13:58:43 -0800

    Solve 07_tempConversion

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let exactC = (fahrenheit - 32) * 5/9;
+  let roundC = Math.round(exactC*10)/10;
+  return roundC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let exactF = (celsius * 9/5) + 32
+  let roundF = Math.round(exactF*10)/10;
+  return roundF;
 };
 
 // Do not edit below this line

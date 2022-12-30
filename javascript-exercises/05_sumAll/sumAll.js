5df5d7e4da2b89dc9dda1a7f6f76af3150c99ee5
Arya Gill <aryagill43@icloud.com>
2022-12-29 21:20:37 -0800

    Solve 05_sumAll

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (Math.sign(start)==-1 || Math.sign(end)==-1) {
+        return "ERROR";
+    } else if (typeof start =="string" || typeof end == "string"){
+        return "ERROR";
+    }
+    else if (start<end) {
+        for (let i = start; i <= end; i++) {
+                sum+=i;
+        }
+    } else if (start>end){
+        for (let i = end; i <= start; i++) {
+            sum+=i;
+        }
+    } else {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line

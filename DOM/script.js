beadf49752934a5f5d3d03c097ffe3fde74f0f9e
Arya Gill <aryagill43@icloud.com>
2023-01-06 23:59:50 -0800

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -3,5 +3,9 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
-
 container.appendChild(content);
+
+const para = document.createElement("p");
+para.style.color = "red";
+para.textContent = "Hey, I'm red";
+container.appendChild(para);
